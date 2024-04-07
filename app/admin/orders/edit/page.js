2c4fef815503084e556d08fbe662da7eb50702b6
zbytes2227 <zbytes0@gmail.com>
2024-04-07 20:52:11 +0530

    fixexd edit modals

--- app/admin/orders/edit/page.js
+++ app/admin/orders/edit/page.js
@@ -18,11 +18,27 @@ const Page = () => {
   ]);
 
 
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isTModalOpen, setisTModalOpen] = useState(false);
 
-  const toggleModal = () => {
-    setIsModalOpen(!isModalOpen);
+  const toggleTModal = () => {
+    setisTModalOpen(!isTModalOpen);
   };
+
+
+
+  const [isPModalOpen, setisPModalOpen] = useState(false);
+
+  const togglePModal = () => {
+    setisPModalOpen(!isPModalOpen);
+  };
+
+
+  const [isCModalOpen, setisCModalOpen] = useState(false);
+
+  const toggleCModal = () => {
+    setisCModalOpen(!isCModalOpen);
+  };
+
 
 
   useEffect(() => {
@@ -423,7 +439,7 @@ const Page = () => {
                   list="country-list"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                 />
-                <button data-modal-target="customer-modal" data-modal-toggle="customer-modal" className="ml-2.5 bg-blue-500 text-white px-3 py-2 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">+</button>
+                <button onClick={toggleCModal} data-modal-target="customer-modal" data-modal-toggle="customer-modal" className="ml-2.5 bg-blue-500 text-white px-3 py-2 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">+</button>
               </div>
 
               <datalist id="country-list">
@@ -685,24 +701,28 @@ const Page = () => {
           <a href="/admin/products/add" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center m-2" type="button">
             New Product
           </a>
-          <button  onClick={toggleModal} data-modal-target="tracking-modal" data-modal-toggle="tracking-modal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center m-2" type="button">
+          <button  onClick={toggleTModal} data-modal-target="tracking-modal" data-modal-toggle="tracking-modal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center m-2" type="button">
             Add Tracking Details
           </button>
 
-          <button data-modal-target="payment-modal" data-modal-toggle="payment-modal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center m-2" type="button"> Add Payment Details </button>
+          <button onClick={togglePModal} data-modal-target="payment-modal" data-modal-toggle="payment-modal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center m-2" type="button"> Add Payment Details </button>
 
           <button onClick={save} class="block text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center m-2" type="button">Save Order Edits </button>
         </div>
 
 
-        <div id="customer-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+        <div id="customer-modal" tabindex="-1"  aria-hidden={!isCModalOpen}
+        className={`${
+          isCModalOpen ? '' : 'hidden'
+        } overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full`}
+      >
           <div class="relative p-4 w-full max-w-md max-h-full">
             <div class="relative bg-white rounded-lg shadow border-4">
               <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t ">
                 <h3 class="text-xl font-semibold text-gray-900">
                   Add New Customer
                 </h3>
-                <button type="button" class="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center " data-modal-hide="customer-modal">
+                <button onClick={toggleCModal} type="button" class="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center " data-modal-hide="customer-modal">
                   <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
                   </svg>
@@ -790,9 +810,9 @@ const Page = () => {
         <div
         id="tracking-modal"
         tabIndex="-1"
-        aria-hidden={!isModalOpen}
+        aria-hidden={!isTModalOpen}
         className={`${
-          isModalOpen ? '' : 'hidden'
+          isTModalOpen ? '' : 'hidden'
         } overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full`}
       >
           <div class="relative p-4 w-full max-w-md max-h-full">
@@ -847,12 +867,16 @@ const Page = () => {
           </div>
         </div>
 
-        <div id="payment-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+        <div id="payment-modal" tabindex="-1"  aria-hidden={!isPModalOpen}
+        className={`${
+          isPModalOpen ? '' : 'hidden'
+        } overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full`}
+      >
           <div class="relative p-4 w-full max-w-md max-h-full">
             <div class="relative bg-white rounded-lg shadow border-4">
               <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t ">
                 <h3 class="text-xl font-semibold text-gray-900"> Payment Details for order </h3>
-                <button type="button" class="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center " data-modal-hide="payment-modal">
+                <button onClick={togglePModal} type="button" class="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center " data-modal-hide="payment-modal">
                   <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
                   </svg>

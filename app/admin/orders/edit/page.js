e174d95cbfedee8a7dbdb1ee7435d1a2a4ff14ef
zbytes2227 <zbytes0@gmail.com>
2024-04-07 20:41:41 +0530

    try to fix

--- app/admin/orders/edit/page.js
+++ app/admin/orders/edit/page.js
@@ -18,6 +18,12 @@ const Page = () => {
   ]);
 
 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
 
   useEffect(() => {
     fetch("/api/getOrder", {
@@ -679,10 +685,12 @@ const Page = () => {
           <a href="/admin/products/add" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center m-2" type="button">
             New Product
           </a>
-          <button data-modal-target="tracking-modal" data-modal-toggle="tracking-modal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center m-2" type="button">
+          <button  onClick={toggleModal} data-modal-target="tracking-modal" data-modal-toggle="tracking-modal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center m-2" type="button">
             Add Tracking Details
           </button>
+
           <button data-modal-target="payment-modal" data-modal-toggle="payment-modal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center m-2" type="button"> Add Payment Details </button>
+
           <button onClick={save} class="block text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center m-2" type="button">Save Order Edits </button>
         </div>
 
@@ -779,9 +787,14 @@ const Page = () => {
           </div>
         </div>
 
-
-
-        <div id="tracking-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+        <div
+        id="tracking-modal"
+        tabIndex="-1"
+        aria-hidden={!isModalOpen}
+        className={`${
+          isModalOpen ? '' : 'hidden'
+        } overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full`}
+      >
           <div class="relative p-4 w-full max-w-md max-h-full">
             <div class="relative bg-white rounded-lg shadow border-4">
               <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t ">
@@ -833,9 +846,6 @@ const Page = () => {
             </div>
           </div>
         </div>
-
-
-
 
         <div id="payment-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
           <div class="relative p-4 w-full max-w-md max-h-full">
--- app/page.js
+++ app/page.js
@@ -49,23 +49,24 @@ export default function Home() {
   return (
     <>
       <div class="p-4 mx-auto container mt-20 text-center">
-        <h1 class="mb-4 text-3xl font-extrabold text-gray-900 md:text-5xl lg:text-6xl">
-          <span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
-            #Smart Invoice
-          </span>{" "}
-          System
-        </h1>
-        <p class="text-lg font-normal text-gray-500 lg:text-xl">
-       -------------------------------------------------------------------------------------------
+     
+        <p class="text-lg  font-semibold text-red-500 lg:text-xl">
+     THIS DATABASE IS TEMPORARY | CONSULT DEVELOPERS BEFORE FEEDING DATA
         </p>
         <div className="mt-10  flex justify-center items-center">
           {/* <Image height={500} width={500} alt=";lknjb" className=" shadow shadow-xl rounded-lg" src={"/images/image.jpeg"} /> */}
         </div>
       </div>
+<div className="border mx-auto container p-4 justify-between text-center">
 
-
-      <div className="mx-auto border container flex justify-between text-center p-5 max-w-max">
-
+      <h1 class="mb-4 text-3xl font-extrabold text-gray-900 md:text-5xl lg:text-6xl">
+          <span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+            #Smart Invoice
+          </span>{" "}
+          System
+        </h1>
+      <div className="mx-auto container flex justify-between text-center p-5 max-w-max">
+     
         <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 mx-2">
           <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Total Customers</h5>
           <p class="font-extrabold text-3xl text-gray-700 ">{Dashboard.Customers}</p>
@@ -90,7 +91,8 @@ export default function Home() {
       {/* <div className="container mx-auto justify-between items-center">
       <a href="/admin/trackings" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">All Trackings</a>
       <a href="/admin/trackings" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">All Payments</a>
-      </div> */}
+    </div> */}
+    </div>
       
     </>
   );
